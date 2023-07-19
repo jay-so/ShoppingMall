6f21bbe91f26e7fa575d8169decacf9f642f7e0e
jay-so <merry9504@naver.com>
2023-07-19 20:16:14 +0900

    feat: ItemDto 생성

---
+++ src/main/java/com/programmers/shoppingMall/dto/ItemDto.java
@@ -0,0 +1,24 @@
+package com.programmers.shoppingMall.dto;
+
+import java.time.LocalDateTime;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class ItemDto {
+
+    private Long id;
+
+    private String itemName;
+
+    private Integer price;
+
+    private String itemDetail;
+
+    private String sellStatCd;
+
+    private LocalDateTime regTime;
+
+    private LocalDateTime updateTime;
+}
