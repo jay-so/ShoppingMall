a7e5b7674824d80b7b92be3e0ff5fd450c3a796a
jay-so <merry9504@naver.com>
2023-07-19 22:30:13 +0900

    refactor: MemberRepository 클래스명 refactoring

--- src/main/java/com/programmers/shoppingMall/repository/MemberRepository.java
+++ src/main/java/com/programmers/shoppingMall/repository/MemberRepository.java
@@ -3,7 +3,7 @@ package com.programmers.shoppingMall.repository;
 import com.programmers.shoppingMall.entity.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface MemberRepostiory extends JpaRepository<Member,Long> {
+public interface MemberRepository extends JpaRepository<Member,Long> {
 
     Member findByEmail(String email);
 
