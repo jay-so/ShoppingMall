7e02bbefda656b8368844088a72683d370e5ae9a
jay-so <merry9504@naver.com>
2023-07-19 21:21:33 +0900

    feat: MemberEntity 생성

---
+++ src/main/java/com/programmers/shoppingMall/entity/Member.java
@@ -0,0 +1,53 @@
+package com.programmers.shoppingMall.entity;
+
+import com.programmers.shoppingMall.dto.MemberFormDto;
+import com.programmers.shoppingMall.enums.Role;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@Entity
+@Table(name="member")
+@Getter
+@Setter
+@ToString
+public class Member{
+
+    @Id
+    @Column(name="member_id")
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id; //멤버 코드
+
+    private String name; //이름
+
+    @Column(unique = true)
+    private String email; //이메일
+
+    private String password; //비밀번호
+
+    private String address; //주소
+
+    @Enumerated(EnumType.STRING)
+    private Role role; //역할
+
+    public static Member createMember(MemberFormDto memberFormDto, PasswordEncoder passwordEncoder){
+        Member member = new Member();
+        member.setName(memberFormDto.getName());
+        member.setEmail(memberFormDto.getEmail());
+        member.setAddress(memberFormDto.getAddress());
+        String password = passwordEncoder.encode(memberFormDto.getPassword());
+        member.setPassword(password);
+        member.setRole(Role.ADMIN);
+        return member;
+    }
+}
+
