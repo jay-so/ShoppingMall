870ed76885daccb054750e57d12b491d87ccf2f0
jay-so <merry9504@naver.com>
2023-07-19 16:23:50 +0900

    feat: findByItemName 메소드 (상품명 검색) 추가

--- src/main/java/com/programmers/shoppingMall/repository/ItemRepository.java
+++ src/main/java/com/programmers/shoppingMall/repository/ItemRepository.java
@@ -1,8 +1,11 @@
 package com.programmers.shoppingMall.repository;
 
 import com.programmers.shoppingMall.entity.Item;
+import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface ItemRepository extends JpaRepository<Item,Long> {
+
+    List<Item> findByItemName(String name);
 
 }
