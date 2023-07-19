3c0bbb4695c82b6d03dd0e7a536b744e9ef392a3
jay-so <merry9504@naver.com>
2023-07-19 16:24:06 +0900

    test: 상품명으로 조회하는 테스트 로직 추가

--- src/test/java/com/programmers/shoppingMall/repository/ItemRepositoryTest.java
+++ src/test/java/com/programmers/shoppingMall/repository/ItemRepositoryTest.java
@@ -5,6 +5,7 @@ import static org.junit.jupiter.api.Assertions.*;
 import com.programmers.shoppingMall.entity.Item;
 import com.programmers.shoppingMall.enums.ItemSellStatus;
 import java.time.LocalDateTime;
+import java.util.List;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -33,4 +34,26 @@ class ItemRepositoryTest {
         System.out.println("savedItem.toString() = " + savedItem.toString());
     }
 
+    public void createItemList(){
+        for(int i=1;i<=10;i++){
+            Item item = new Item();
+            item.setItemName("테스트 상품" + i);
+            item.setPrice(10000 + i);
+            item.setItemDetail("테스트 상품 상세 설명" + i);
+            item.setItemSellStatus(ItemSellStatus.SELL);
+            item.setStockNumber(100); item.setRegTime(LocalDateTime.now());
+            item.setUpdateTime(LocalDateTime.now());
+            Item savedItem = itemRepository.save(item);
+        }
+    }
+
+    @Test
+    @DisplayName("상품명 조회 테스트")
+    public void findByItemNmTest(){
+        this.createItemList();
+        List<Item> itemList = itemRepository.findByItemName("테스트 상품1");
+        for(Item item : itemList){
+            System.out.println("찾는 상품 데이터 : " + item.toString());
+        }
+    }
 }
