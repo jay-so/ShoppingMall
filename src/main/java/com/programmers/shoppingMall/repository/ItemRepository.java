29201be0c002d7f330fd1391346cc025da32bcd0
jay-so <merry9504@naver.com>
2023-07-19 20:10:17 +0900

    feat: findByItemDetail(가격 내림차순으로 상품 상세 설명을 조회하는 JPQL 테스트)

--- src/main/java/com/programmers/shoppingMall/repository/ItemRepository.java
+++ src/main/java/com/programmers/shoppingMall/repository/ItemRepository.java
@@ -3,6 +3,8 @@ package com.programmers.shoppingMall.repository;
 import com.programmers.shoppingMall.entity.Item;
 import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface ItemRepository extends JpaRepository<Item,Long> {
 
@@ -13,4 +15,7 @@ public interface ItemRepository extends JpaRepository<Item,Long> {
     List<Item> findByPriceLessThan(Integer price);
 
     List<Item> findByPriceLessThanOrderByPriceDesc(Integer price);
+
+    @Query("select i from Item i where i.itemDetail like %:itemDetail% order by i.price desc")
+    List<Item> findByItemDetail(@Param("itemDetail") String itemDetail);
 }
