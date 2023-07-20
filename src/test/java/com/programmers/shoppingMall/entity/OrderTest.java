4f597e8673a5530e79e1900ef8fa877fae7db629
jay-so <merry9504@naver.com>
2023-07-20 22:55:41 +0900

    test: Order 엔티티 (고아 객체 제거 테스트) test

--- src/test/java/com/programmers/shoppingMall/entity/OrderTest.java
+++ src/test/java/com/programmers/shoppingMall/entity/OrderTest.java
@@ -4,6 +4,7 @@ import static org.junit.jupiter.api.Assertions.*;
 
 import com.programmers.shoppingMall.enums.ItemSellStatus;
 import com.programmers.shoppingMall.repository.ItemRepository;
+import com.programmers.shoppingMall.repository.MemberRepository;
 import com.programmers.shoppingMall.repository.OrderRepository;
 import java.time.LocalDateTime;
 import javax.persistence.EntityManager;
@@ -26,6 +27,9 @@ class OrderTest {
 
     @Autowired
     ItemRepository itemRepository;
+
+    @Autowired
+    MemberRepository memberRepository;
 
     @PersistenceContext
     EntityManager em;
@@ -66,5 +70,30 @@ class OrderTest {
         Order savedOrder = orderRepository.findById(order.getId())
                 .orElseThrow(EntityNotFoundException::new);
         assertEquals(3, savedOrder.getOrderItems().size());
+    }
+    public Order createOrder(){
+        Order order = new Order();
+        for(int i=0;i<3;i++){
+            Item item = createItem();
+            itemRepository.save(item);
+            OrderItem orderItem = new OrderItem();
+            orderItem.setItem(item);
+            orderItem.setCount(10);
+            orderItem.setOrderPrice(1000);
+            orderItem.setOrder(order);
+            order.getOrderItems().add(orderItem);
+        }
+        Member member = new Member();
+        memberRepository.save(member);
+        order.setMember(member);
+        orderRepository.save(order);
+        return order;
+    }
+    @Test
+    @DisplayName("고아객체 제거 테스트")
+    public void orphanRemovalTest(){
+        Order order = this.createOrder();
+        order.getOrderItems().remove(0);
+        em.flush();
     }
 }
