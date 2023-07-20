a5fde91060dbc884be81fc01547a9576b33b1ac0
jay-so <merry9504@naver.com>
2023-07-20 22:29:32 +0900

    feat: CartItem 엔티티 구현

---
+++ src/main/java/com/programmers/shoppingMall/entity/CartItem.java
@@ -0,0 +1,35 @@
+package com.programmers.shoppingMall.entity;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Entity
+@Table(name = "cart_item")
+@Getter
+@Setter
+@ToString
+public class CartItem {
+
+    @Id
+    @GeneratedValue
+    @Column(name = "cart_item_id")
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "cart_id")
+    private Cart cart;
+
+    @ManyToOne
+    @JoinColumn(name = "item_id")
+    private Item item;
+
+    private int count;
+}
