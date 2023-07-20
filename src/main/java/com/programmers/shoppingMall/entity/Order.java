a8f7c88c7d00037f02c008618cccbc6160acbd82
jay-so <merry9504@naver.com>
2023-07-20 22:38:31 +0900

    feat: Order 엔티티 구현

---
+++ src/main/java/com/programmers/shoppingMall/entity/Order.java
@@ -0,0 +1,48 @@
+package com.programmers.shoppingMall.entity;
+
+import com.programmers.shoppingMall.enums.OrderStatus;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Entity
+@Table(name = "orders")
+@Getter
+@Setter
+@ToString
+public class Order {
+
+    @Id
+    @GeneratedValue
+    @Column(name = "order_id")
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "member_id")
+    private Member member;
+
+    private LocalDateTime orderDate; //주문 날짜
+
+    @Enumerated(EnumType.STRING)
+    private OrderStatus orderStatus; //주문 상태
+
+    @OneToMany(mappedBy = "order")
+    private List<OrderItem> orderItems = new ArrayList<>();
+
+    private LocalDateTime regTime;
+
+    private LocalDateTime updateTime;
+}
