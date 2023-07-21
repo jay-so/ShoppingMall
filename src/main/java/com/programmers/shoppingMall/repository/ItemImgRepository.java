95138c5f5184a3ab1e6c3478b02bb9308c8f3f5f
jay-so <merry9504@naver.com>
2023-07-21 23:43:59 +0900

    refactor: ItemImgRepository refactoring

--- src/main/java/com/programmers/shoppingMall/repository/ItemImgRepository.java
+++ src/main/java/com/programmers/shoppingMall/repository/ItemImgRepository.java
@@ -1,8 +1,11 @@
 package com.programmers.shoppingMall.repository;
 
 import com.programmers.shoppingMall.entity.ItemImg;
+import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface ItemImgRepository extends JpaRepository<ItemImg, Long> {
+
+    List<ItemImg> findByItemIdOrderByIdAsc(Long itemId);
 
 }
