f8f30ab7613ae23f70f2cbc9304830373b41fd3c
jay-so <merry9504@naver.com>
2023-07-21 23:18:30 +0900

    feat:ItemImgRepository 구현

---
+++ src/main/java/com/programmers/shoppingMall/repository/ItemImgRepository.java
@@ -0,0 +1,8 @@
+package com.programmers.shoppingMall.repository;
+
+import com.programmers.shoppingMall.entity.ItemImg;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ItemImgRepository extends JpaRepository<ItemImg, Long> {
+
+}
