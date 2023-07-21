f3daee8e2af84cfd09a620ce3f95777de26a17be
jay-so <merry9504@naver.com>
2023-07-21 20:00:08 +0900

    refactor: Item 엔티티 refatoring(BaseEntity 상속)

--- src/main/java/com/programmers/shoppingMall/entity/Item.java
+++ src/main/java/com/programmers/shoppingMall/entity/Item.java
@@ -20,7 +20,7 @@ import lombok.ToString;
 @Getter
 @Setter
 @ToString
-public class Item {
+public class Item extends BaseEntity{
 
     @Id
     @Column(name = "item_id")
@@ -43,7 +43,4 @@ public class Item {
     @Enumerated(EnumType.STRING)
     private ItemSellStatus itemSellStatus;//상품 판매 상태
 
-    private LocalDateTime regTime;//상품 등록 시간
-
-    private LocalDateTime updateTime; //상품 수정 시간
 }
