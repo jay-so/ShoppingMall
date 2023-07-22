c02bfb1c303154ebe0b5ffb0ebad17dc50806487
jay-so <merry9504@naver.com>
2023-07-22 21:54:47 +0900

    refactor: CartItem refactoring

--- src/main/java/com/programmers/shoppingMall/entity/CartItem.java
+++ src/main/java/com/programmers/shoppingMall/entity/CartItem.java
@@ -4,6 +4,7 @@ import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
@@ -20,7 +21,7 @@ import lombok.ToString;
 public class CartItem extends BaseEntity{
 
     @Id
-    @GeneratedValue
+    @GeneratedValue(strategy = GenerationType.AUTO)
     @Column(name = "cart_item_id")
     private Long id;
 
