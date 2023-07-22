c746da895794ae2bad8509edb0db4d0157a977fc
jay-so <merry9504@naver.com>
2023-07-22 16:31:05 +0900

    refator: getAdminItemPage 추가

--- src/main/java/com/programmers/shoppingMall/service/ItemService.java
+++ src/main/java/com/programmers/shoppingMall/service/ItemService.java
@@ -1,12 +1,18 @@
 package com.programmers.shoppingMall.service;
 
 import com.programmers.shoppingMall.dto.ItemFormDto;
+import com.programmers.shoppingMall.dto.ItemImgDto;
+import com.programmers.shoppingMall.dto.ItemSearchDto;
+import com.programmers.shoppingMall.dto.MainItemDto;
 import com.programmers.shoppingMall.entity.Item;
 import com.programmers.shoppingMall.entity.ItemImg;
 import com.programmers.shoppingMall.repository.ItemImgRepository;
 import com.programmers.shoppingMall.repository.ItemRepository;
+import java.util.ArrayList;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.multipart.MultipartFile;
@@ -43,4 +49,8 @@ public class ItemService {
         return item.getId();
     }
 
+    @Transactional(readOnly = true)
+    public Page<Item> getAdminItemPage(ItemSearchDto itemSearchDto, Pageable pageable){
+        return itemRepository.getAdminItemPage(itemSearchDto, pageable);
+    }
 }
