39952a9424dd0894d17dcdb408ca13d6f1c41539
jay-so <merry9504@naver.com>
2023-07-22 16:32:16 +0900

    refactor: ItemController refactoring

--- src/main/java/com/programmers/shoppingMall/controller/ItemController.java
+++ src/main/java/com/programmers/shoppingMall/controller/ItemController.java
@@ -1,14 +1,21 @@
 package com.programmers.shoppingMall.controller;
 
 import com.programmers.shoppingMall.dto.ItemFormDto;
+import com.programmers.shoppingMall.dto.ItemSearchDto;
+import com.programmers.shoppingMall.entity.Item;
 import com.programmers.shoppingMall.service.ItemService;
 import java.util.List;
+import java.util.Optional;
 import javax.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.multipart.MultipartFile;
@@ -43,5 +50,18 @@ public class ItemController {
         }
 
         return "redirect:/";
+    }
+
+    @GetMapping(value = {"/admin/items", "/admin/items/{page}"})
+    public String itemManage(ItemSearchDto itemSearchDto, @PathVariable("page") Optional<Integer> page, Model model){
+
+        Pageable pageable = PageRequest.of(page.isPresent() ? page.get() : 0, 3);
+        Page<Item> items = itemService.getAdminItemPage(itemSearchDto, pageable);
+
+        model.addAttribute("items", items);
+        model.addAttribute("itemSearchDto", itemSearchDto);
+        model.addAttribute("maxPage", 5);
+
+        return "item/itemMng";
     }
 }
