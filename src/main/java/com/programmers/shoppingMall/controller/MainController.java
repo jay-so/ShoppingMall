6ad4333da19736ca5f5ebab93087b88889170bd2
jay-so <merry9504@naver.com>
2023-07-22 21:20:15 +0900

    refactor: MainController refactoring

--- src/main/java/com/programmers/shoppingMall/controller/MainController.java
+++ src/main/java/com/programmers/shoppingMall/controller/MainController.java
@@ -1,13 +1,32 @@
 package com.programmers.shoppingMall.controller;
 
+import com.programmers.shoppingMall.dto.ItemSearchDto;
+import com.programmers.shoppingMall.dto.MainItemDto;
+import com.programmers.shoppingMall.service.ItemService;
+import java.util.Optional;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
+@RequiredArgsConstructor
 public class MainController {
 
+    private final ItemService itemService;
+
     @GetMapping(value = "/")
-    public String main(){
+    public String main(ItemSearchDto itemSearchDto, Optional<Integer> page, Model model){
+
+        Pageable pageable = PageRequest.of(page.isPresent() ? page.get() : 0, 6);
+        Page<MainItemDto> items = itemService.getMainItemPage(itemSearchDto, pageable);
+
+        model.addAttribute("items", items);
+        model.addAttribute("itemSearchDto", itemSearchDto);
+        model.addAttribute("maxPage", 5);
         return "main";
     }
 }
